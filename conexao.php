9f5042214bcd7ba192a016160f2ddc831fa0d10f
AndersonBHBR <anderson.bhbr@gmail.com>
2016-11-26 10:48:23 -0200

    verificar

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 function conectar(){
     $URL = "localhost";
     $USUARIO = "andersonbhbr";
-    $SENHA = "loverei169";
+    $SENHA = "12345";
     $DATABASE = "escola";
 
     $con = new mysqli($URL, $USUARIO, $SENHA, $DATABASE);
